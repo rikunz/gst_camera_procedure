64e3e9b31644c547cf809c04ce34be634bd13fe3
rikunz <thoriqfirdaus8@gmail.com>
2025-07-14 09:15:25 +0700

    fix: update framerate in camera procedure and add consumer TCP sink script

---
+++ consumer-infered-sink-tcp.sh
@@ -0,0 +1,5 @@
+gst-launch-1.0 tcpclientsrc host=127.0.0.1 port=9000 ! \
+    multipartdemux ! \
+    jpegdec ! \
+    videoconvert ! \
+    fpsdisplaysink video-sink=autovideosink text-overlay=true sync=false

e4beac3f2a7feff6ddf1f506d18ba6035d46c2b4
rikunz <thoriqfirdaus8@gmail.com>
2025-07-14 21:58:51 +0700

    fix: update buffer size and add videoconvert in camera procedure script

--- camera_procedure.sh
+++ camera_procedure.sh
@@ -1,6 +1,6 @@
 gst-launch-1.0 libcamerasrc ! \
-    video/x-raw,width=1920,height=1080,framerate=10/1,format=NV12 ! \
-    queue max-size-buffers=2 leaky=downstream ! \
-    videorate ! \
-    video/x-raw,framerate=10/1 ! \
-    shmsink socket-path=/tmp/feed.raw sync=false wait-for-connection=false
+	video/x-raw,width=1920,height=1080,framerate=10/1,format=NV12 ! \
+	queue max-size-buffers=10 leaky=downstream ! \
+	videoconvert ! \
+	identity silent=false ! \
+	shmsink socket-path=/tmp/feed.raw sync=false wait-for-connection=false

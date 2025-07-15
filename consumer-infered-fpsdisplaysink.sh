7874c0204d6e32fc58bb4fa0e75dd97b558b6b13
rikunz <thoriqfirdaus8@gmail.com>
2025-07-15 10:28:17 +0700

    fix: update buffer size in fpsdisplaysink scripts

--- consumer-infered-fpsdisplaysink.sh
+++ consumer-infered-fpsdisplaysink.sh
@@ -1,6 +1,6 @@
 gst-launch-1.0 \
-  shmsrc socket-path=/tmp/infered.feed do-timestamp=true is-live=true ! \
+  shmsrc socket-path=/tmp/infered.feed is-live=true ! \
   video/x-raw,format=RGB,width=1920,height=1080,framerate=10/1 ! \
-  queue max-size-buffers=2 leaky=upstream ! \
+  queue max-size-buffers=10 leaky=downstream ! \
   videoconvert ! \
-  fpsdisplaysink video-sink=autovideosink sync=false text-overlay=true
+  fpsdisplaysink video-sink=autovideosink sync=false
--- consumer-raw-fpsdisplaysink.sh
+++ consumer-raw-fpsdisplaysink.sh
@@ -1,6 +1,6 @@
 gst-launch-1.0 shmsrc socket-path=/tmp/feed.raw do-timestamp=1 ! \
 	video/x-raw,format=NV12,width=1920,height=1080,framerate=30/1 ! \
 	identity sync=true ! \
-	queue max-size-buffers=2 leaky=downstream ! \
+	queue max-size-buffers=10 leaky=downstream ! \
 	videoconvert ! \
-	fpsdisplaysink video-sink=autovideosink text-overlay=true sync=false
+	fpsdisplaysink video-sink=autovideosink  sync=false
